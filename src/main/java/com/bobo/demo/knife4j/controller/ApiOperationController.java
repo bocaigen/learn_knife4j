5a842b6511b1e1023d95dcd062c00d8404e5eb30
xibo <1483964677@qq.com>
2021-01-13 09:51:09 +0800

    修改一些描述

--- src/main/java/com/bobo/demo/knife4j/controller/ApiOperationController.java
+++ src/main/java/com/bobo/demo/knife4j/controller/ApiOperationController.java
@@ -20,52 +20,58 @@ public class ApiOperationController {
      * 1、用于方法
      * 2、表示一个http请求的操作
      * 3、属性：
-     * value：用于方法描述，
+     * value：列表名称，用于方法描述，
      * notes: 接口描述，用于提示内容
-     * tags：可以重新分组（视情况而用）
+     * tags：分组列表名称，可以重新分组（视情况而用）
      *
      */
     @ApiOperation(value = "查找用户",notes = "根据用户名查找用户",tags = "用户模块")
     @ApiImplicitParam(name = "username",value = "用户名",defaultValue = "张三")
     @GetMapping("/findUser")
     public ResponseEntity<String> findUser(String username){
-        return ResponseEntity.ok("查找用户"+username);
+        return ResponseEntity.ok("查找用户："+username);
     }
 
     @ApiOperation(value = "添加用户",tags = "用户模块")
+    @ApiImplicitParam(name = "username",value = "用户名",defaultValue = "张三")
     @PostMapping("/insertUser")
-    public ResponseEntity<String> insertUser(){
-        return ResponseEntity.ok("添加用户");
+    public ResponseEntity<String> insertUser(String username){
+        return ResponseEntity.ok("添加用户："+username);
     }
 
     @ApiOperation(value = "修改用户",tags = "用户模块")
+    @ApiImplicitParam(name = "username",value = "用户名",defaultValue = "张三")
     @GetMapping("/modifyUser")
-    public ResponseEntity<String> modifyUser(){
-        return ResponseEntity.ok("修改用户");
+    public ResponseEntity<String> modifyUser(String username){
+        return ResponseEntity.ok("修改用户："+username);
     }
 
     @ApiOperation(value = "删除用户",tags = "用户模块")
+    @ApiImplicitParam(name = "username",value = "用户名",defaultValue = "张三")
     @GetMapping("/deleteUser")
-    public ResponseEntity<String> deleteUser(){
-        return ResponseEntity.ok("删除用户");
+    public ResponseEntity<String> deleteUser(String username){
+        return ResponseEntity.ok("删除用户："+username);
     }
 
     @ApiOperation(value = "查找订单",tags = "订单模块")
+    @ApiImplicitParam(name = "orderCode",value = "订单号",defaultValue = "001")
     @GetMapping("/findOrder")
-    public ResponseEntity<String> findOrder(){
-        return ResponseEntity.ok("查找订单");
+    public ResponseEntity<String> findOrder(String orderCode){
+        return ResponseEntity.ok("查找订单"+orderCode);
     }
 
     @ApiOperation(value = "添加订单",tags = "订单模块")
+    @ApiImplicitParam(name = "username",value = "下单账号",defaultValue = "18937196209")
     @PostMapping("/insertOrder")
-    public ResponseEntity<String> insertOrder(){
-        return ResponseEntity.ok("添加订单");
+    public ResponseEntity<String> insertOrder(String username){
+        return ResponseEntity.ok("添加订单"+username);
     }
 
     @ApiOperation(value = "修改订单",tags = "订单模块")
+    @ApiImplicitParam(name = "orderCode",value = "订单号",defaultValue = "002")
     @GetMapping("/modifyOrder")
-    public ResponseEntity<String> modifyOrder(){
-        return ResponseEntity.ok("修改订单");
+    public ResponseEntity<String> modifyOrder(String orderCode){
+        return ResponseEntity.ok("修改订单"+orderCode);
     }
 
     @ApiOperation(value = "删除订单",notes = "根据订单Id删除订单",tags = "订单模块")
